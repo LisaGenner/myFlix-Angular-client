ad71f8cb166b5cc7f93926d4aa4d0d28596b0f95
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-07-14 16:40:52 -0500

    6.4 implemented movie Card files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,10 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+  
 
   constructor(public dialog: MatDialog) { }
+
 // This is the function that will open the dialog when the signup button is clicked  
 openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
@@ -19,4 +22,16 @@ openUserRegistrationDialog(): void {
     width: '280px'
     });
   }
+openUserLoginDialog(): void {
+  this.dialog.open(MovieCardComponent, {
+    width: '500px'
+  })
+}
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
+    MovieCardComponent,
    
   ],
   imports: [
